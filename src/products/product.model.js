29ab6d03d3284c461dab68b92b5774d353ba07a7
Chandan <indspunk@gmail.com>
2022-11-10 02:29:04 +0530

    size Removed

--- src/products/product.model.js
+++ src/products/product.model.js
@@ -6,7 +6,6 @@ const productSchema = new mongoose.Schema({
     description: { type: String, required: true },
     images: { type: Array, required: true },
     category: { type: String, required: true },
-    size: { type: String, required: true },
     color: { type: String, required: true },
 });
 
